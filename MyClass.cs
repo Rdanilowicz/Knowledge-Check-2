81a63ca4e57e92be7ad0a95623b8997524855f24
rdanilowicz <rjdanilowicz@gmail.com>
2024-09-12 07:35:52 -0400

    I did it!

---
+++ MyClass.cs
@@ -0,0 +1,24 @@
+﻿namespace Knowledge_Check_2
+{
+	public class MyClass
+	{
+		public virtual string Property { get; set; }
+		public override string ToString()
+		{
+			return $"Property: {Property}";
+		}
+	}
+
+	/*ChildClass inherits from MyClass so it can call the string "Property". both properties have overrides so ToString() can include the values
+	of both properties to the list.*/
+
+	public class ChildClass : MyClass
+	{
+		public string AdditionalProperty { get; set; }
+
+		public override string ToString()
+		{
+			return $"Property: {Property}, Additional Property: {AdditionalProperty}";
+		}
+	}
+}
---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿namespace Knowledge_Check_2
+{
+	public class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("How many records do you want to add? ");
+			var numberOfRecords = int.Parse(Console.ReadLine());
+
+			var recordList = new List<ChildClass>();
+			for (int i = 0; i < numberOfRecords; i++)
+			{
+				// In this loop, populate the object's properties using Console.ReadLine()
+				
+				// Create instance of childclass
+				var childclass = new ChildClass();
+
+				Console.WriteLine("Enter the value for Property: ");
+				childclass.Property = Console.ReadLine();
+
+				Console.WriteLine("Enter the value for Additional Property: ");
+				childclass.AdditionalProperty = Console.ReadLine();
+
+				// Adds objects to the list
+				recordList.Add(childclass);
+			}
+
+			// Print out the list of records using Console.WriteLine()
+			foreach (var record in recordList)
+			{
+				Console.WriteLine(record.ToString());
+			}
+			Console.ReadKey();
+		}
+	}
+}
